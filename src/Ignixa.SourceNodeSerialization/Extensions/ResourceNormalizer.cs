d646569c359867db53ff41d36bbfd82ecb3ed55f
Brendan Kowitz <brendan.kowitz@gmail.com>
2025-10-28 14:32:35 -0700

    fix cast

--- src/Ignixa.SourceNodeSerialization/Extensions/ResourceNormalizer.cs
+++ src/Ignixa.SourceNodeSerialization/Extensions/ResourceNormalizer.cs
@@ -34,7 +34,7 @@ public static class ResourceNormalizer
         var reader = new Utf8JsonReader(resourceBytes.Span);
         using var output = _memoryStreamManager.GetStream("resource-normalization");
 
-        using var writer = new Utf8JsonWriter(output);
+        using var writer = new Utf8JsonWriter((Stream)output);
         CopyResourceWithoutVersionMetadata(ref reader, writer);
         writer.Flush();
         output.TryGetBuffer(out ArraySegment<byte> buffer);
